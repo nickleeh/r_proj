591092fde01a55d60569186cc2fe9bcb3cd1c881
Nick Lee <nickleeh@foxmail.com>
2015-08-13 16:30:20 +0800

    Refactored 'zodiac.R' for consistant style and easy to read.

--- zodiac_ems2/zodiac.R
+++ zodiac_ems2/zodiac.R
@@ -2,28 +2,29 @@ library("ggplot2")
 library("data.table")
 library("DescTools")
 
-## Read in data as data.frame
-## ems02 <- read.csv("zodiac_ems2/ems02csv.csv")
+### (1) Read in data as data.frame
+setwd("c:/users/nick/projects/r_proj/zodiac_ems2")
+dt <- fread("ems02csv.csv")
 
-DT <- fread("ems02csv.csv")
-## str(DT)
-## DT <- data.table(ems02)
-
-DT$DOB <- as.Date(DT$DOB,format="%Y/%m/%d")                  # Convert DOB from factor to date format
+### (2) Process
+## reformat date
+dt$DOB <- as.Date(dt$DOB,format="%Y/%m/%d")              # Convert DOB from factor to date format
 
 ## Determine Zodiac symbols according to DOB
-DT <- DT[,.(Name = Name, ZS=Zodiac(DOB)),]                   # Determine zodiac symbols
-DTZQ <- DT[,.(QTY=length(Name)),by=ZS]                       # Count quatity by zodiac symbols
-DTZQ$ZS <- factor(DTZQ$ZS, levels=DTZQ$ZS[order(DTZQ$QTY)])  # Set ZS factor order -- for plot order in the next step.
+dt <- dt[,.(Name = Name, ZodiacSign=Zodiac(DOB)),]       # Determine zodiac signs
+zodiac_qty_dt <- DT[,.(QTY=length(Name)),by=ZodiacSign]  # Count quatity by zodiac symbols
 
-## Plot 
-p <- ggplot(data=DTZQ, aes(x=ZS, y=QTY)) +
+## Set ZodiacSign factor order -- for plot order in the next step.
+zodiac_qty_dt$ZodiacSign <- factor(zodiac_qty_dt$ZodiacSign, levels=zodiac_qty_dt$ZodiacSign[order(zodiac_qty_dt$QTY)])
+
+### (3) Plot 
+p <- ggplot(data=zodiac_qty_dt, aes(x=ZodiacSign, y=QTY)) +
     geom_bar(stat="identity", fill="#009E73") +
     coord_flip() +
     theme(axis.text.y = element_text(colour = 'black', size=rel(1.5)),
           axis.text.x = element_text(colour = 'black', size=rel(1.5)),
           plot.title= element_text(size=18)) + 
-    ggtitle("Zodiac Symbols of English Majors 2") +
+    ggtitle("Zodiac Signs of English Majors 2") +
     labs(y="",x="")
 
 ## Save plot image
